0da88526c3305194b97d8627ad3e987b026ab0a8
felipetrentin <felipetrentintecnico@hotmail.com>
2022-06-12 01:11:51 -0300

    TODAS MIDIAS OPERANTES!

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ console.log('Hello World!'); //just a silly debug
  */
 const Commands = {
 	OpenByUser: "manual-open",
-	OpenByCard: "1",
-	OpenByFace: "2",
+	OpenByCard: "card-open",
+	OpenByFace: "face-open",
 	CardDenied: "3",
     FaceDenied: "4",
     Message:"5"
@@ -26,26 +26,36 @@ socket.onmessage = function (e) {
     /*this runs upon receiving message, we expect a json*/
     console.log('Server: ' + e.data);
     var req = JSON.parse(e.data); //parse data
+    //get elements
+    var mydiv = document.getElementById("feedback-div")
+    var myVideo = mydiv.getElementsByTagName("video")[0];
+    var myMessage = mydiv.getElementsByTagName("p")[0];
     switch(req.event){
         case Commands.OpenByUser:
-            var mydiv = document.getElementById("feedback-div")
-            var myVideo = mydiv.getElementsByTagName("video")[0];
-            var myMessage = mydiv.getElementsByTagName("p")[0];
-            //finds and sets source value, loading the video
+            //finds and sets source value
             mydiv.getElementsByTagName("source")[0].src = "media/cadeado_aberto0001-0120.mp4";
-            myVideo.load();
-
-            myMessage.innerText = req.message
-            //show div
-            mydiv.style.display = 'block';
-            myVideo.onended = function(e) {
-                /*its done! hide!*/
-                mydiv.style.display = 'none';
-            };
-            //reset and play video
-            myVideo.currentTime = 0;
-            myVideo.play();
-            console.log("ooohmygod!")
-            console.log(req.message)
-    }
+            break;
+        case Commands.OpenByCard:
+            //finds and sets source value
+            mydiv.getElementsByTagName("source")[0].src = "media/cadeado_aberto_cartao0001-0120.mp4";
+            break;
+        case Commands.OpenByFace:
+            //finds and sets source value
+            mydiv.getElementsByTagName("source")[0].src = "media/cadeado_aberto_face0001-0120.mp4";
+            break;
+        default:
+            return;
+    
+    };
+    myVideo.load();
+    myMessage.innerText = req.message
+    //show div
+    mydiv.style.display = 'block';
+    myVideo.onended = function(e) {
+        /*its done! hide!*/
+        mydiv.style.display = 'none';
+    };
+    //reset and play video
+    myVideo.currentTime = 0;
+    myVideo.play();
 };
