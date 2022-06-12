22298282c19521b8934841801231fa28a40b7ec4
felipetrentin <felipetrentintecnico@hotmail.com>
2022-06-12 00:59:51 -0300

    First version of media control

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-console.log('Hello World!');
+console.log('Hello World!'); //just a silly debug
 
+/**
+ * Here you are going to define all the commands
+ * it is important for the string to be the same as what the websocket provides.
+ */
 const Commands = {
 	OpenByUser: "manual-open",
 	OpenByCard: "1",
@@ -12,6 +16,8 @@ const Commands = {
 /*connect to JARVIS websocket*/
 var socket = new WebSocket('ws://raspi2server:1880/term/socket');
 
+
+
 socket.onopen = function (event) {
     console.log('WebSocket is connected! Protocol:' + socket.protocol);
 };
@@ -22,6 +28,23 @@ socket.onmessage = function (e) {
     var req = JSON.parse(e.data); //parse data
     switch(req.event){
         case Commands.OpenByUser:
+            var mydiv = document.getElementById("feedback-div")
+            var myVideo = mydiv.getElementsByTagName("video")[0];
+            var myMessage = mydiv.getElementsByTagName("p")[0];
+            //finds and sets source value, loading the video
+            mydiv.getElementsByTagName("source")[0].src = "media/cadeado_aberto0001-0120.mp4";
+            myVideo.load();
+
+            myMessage.innerText = req.message
+            //show div
+            mydiv.style.display = 'block';
+            myVideo.onended = function(e) {
+                /*its done! hide!*/
+                mydiv.style.display = 'none';
+            };
+            //reset and play video
+            myVideo.currentTime = 0;
+            myVideo.play();
             console.log("ooohmygod!")
             console.log(req.message)
     }
